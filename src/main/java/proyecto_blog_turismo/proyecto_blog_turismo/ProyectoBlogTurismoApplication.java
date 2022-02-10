f4dbdf93b8a13e756caa3eb456b227a90a7dbe33
Fdylan05 <dfigueroa00286@ufide.ac.cr>
2022-02-10 12:17:56 -0600

    Primer commit del proyecto Se agrega la pagina login Se agraga login.css TODO:modificar diseño login

---
+++ src/main/java/proyecto_blog_turismo/proyecto_blog_turismo/ProyectoBlogTurismoApplication.java
@@ -0,0 +1,13 @@
+package proyecto_blog_turismo.proyecto_blog_turismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBlogTurismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBlogTurismoApplication.class, args);
+	}
+
+}
