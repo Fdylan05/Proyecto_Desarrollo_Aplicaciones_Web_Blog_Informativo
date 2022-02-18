79053362191f75142377b4c5f8684038e742c8c1
Dylan <Dylan@DESKTOP-N2NGT2F>
2022-02-17 18:08:45 -0600

    Se agrega menu principal

---
+++ README.md
@@ -0,0 +1,6 @@
+# Proyecto_Desarrollo_Aplicaciones_Web_Blog_Informativo
+Proyecto del curso de Desarrollo de aplicaciones web y patrones.
+Integrantes: 
+- Dylan Figueroa Mena
+- Natalia Abarca Madrigal
+- Johann Segura Chacon
---
+++ src/test/java/proyecto_blog_turismo/proyecto_blog_turismo/ProyectoBlogTurismoApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto_blog_turismo.proyecto_blog_turismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBlogTurismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
